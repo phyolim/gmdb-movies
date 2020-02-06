c7451a1345a8ff3b6f89015425c71612e221f73d
Phyo Lim <phyo.lim@mckesson.com>
2020-02-06 12:02:30 -0500

    Setup MovieController

--- src/main/java/com/galvanize/gmdbmovies/controllers/MovieController.java
+++ src/main/java/com/galvanize/gmdbmovies/controllers/MovieController.java
@@ -1,4 +1,8 @@
 package com.galvanize.gmdbmovies.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MovieController {
+
 }
--- src/test/java/com/galvanize/gmdbmovies/controllers/MovieControllerTest.java
+++ src/test/java/com/galvanize/gmdbmovies/controllers/MovieControllerTest.java
@@ -1,7 +1,29 @@
 package com.galvanize.gmdbmovies.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class MovieControllerTest {
+    private static MovieController movieController;
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testMoviesEndpointExist() throws Exception {
+        String url = "/movies";
+        mvc.perform(get(url))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 
 }
